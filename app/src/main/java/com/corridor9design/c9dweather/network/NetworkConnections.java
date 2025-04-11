144d3bbf82d54857c3decefa92a690d985ff06cb
Andrew Buskov <andrew.buskov@gmail.com>
2025-04-11 07:48:30 -0500

    Finish Network Connections

--- app/src/main/java/com/corridor9design/c9dweather/network/NetworkConnections.java
+++ app/src/main/java/com/corridor9design/c9dweather/network/NetworkConnections.java
@@ -8,4 +8,11 @@ public class NetworkConnections {
     ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
         Network nw = connectivityManager.getActiveNetwork();
+        if (nw == null) return false;
+        NetworkCapabilities actNw = connectivityManager.getNetworkCapabilities(nw);
+        return actNw != null && (actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH));
+    } else {
+        NetworkInfo nwInfo = connectivityManager.getActiveNetworkInfo();
+        return nwInfo != null && nwInfo.isConnected();
+    }
 }
